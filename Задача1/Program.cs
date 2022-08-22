32c2cdb509063353110fef92152fcdcec779b140
Olga <olyakapran@yandex.by>
2022-08-22 17:13:11 +0300

    second commit

---
+++ Задача1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задача1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1>num2)
+            {
+                Console.WriteLine("max = "+num1);
+            }
+            else
+            {
+                Console.WriteLine("max = "+num2);
+            };
+        }
+    }
+}
