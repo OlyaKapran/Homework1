3e19610e9b3f02240b957955e4a353f0c5c9f58c
Olga <olyakapran@yandex.by>
2022-08-22 17:29:15 +0300

    4 commit

---
+++ Задача3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Задача3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num %2==0)
+            {
+                Console.WriteLine(num+ " = Четное число" );
+            }
+            else
+            {
+                Console.WriteLine(num+ " = Нечетное число" );
+            }
+
+        }
+    }
+}
